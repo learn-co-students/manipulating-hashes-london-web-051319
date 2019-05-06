a3fafe683abb22f3c6de11efe810374fd6488d19
Catherine Maritz <catherinemaritz1@gmail.com>
2019-05-06 11:36:04 +0100

    code along-manipulating nested hashes tidy code

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,29 +13,6 @@ def first_challenge
     }
   }
 
-#   #your code here
-#   # contacts.each do |person, data|
-#   #   puts "#{person}: #{data}"
-#     data.each do |info, value|
-#       puts "#{info}: #{value}"
-#       if info == :favorite_icecream_flavors
-#         value.each do |flavor|
-#           puts "#{flavor}"
-#           if flavor.includes?("strawberry")
-#             flavor.delete("strawberry")
-#           end
-#         end
-#       end 
-#     end
-
-#   #remember to return your newly altered contacts hash!
-#   contacts
-# end
-# end
-
-  #your code here
-  # contacts.each do |person, data|
-  #   puts "#{person}: #{data}"
 contacts["Freddy Mercury"].each do |info, value|
   puts "#{info}: #{value}"
   if info == :favorite_icecream_flavors
